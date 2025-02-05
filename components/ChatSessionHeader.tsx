dba3917de2c4344e0cc306b297104fe9c513fce7
Amaan Alam <amaan@arivani.net>
2025-02-05 20:46:45 +0530

    chatSessionHeader

--- components/ChatSessionHeader.tsx
+++ components/ChatSessionHeader.tsx
@@ -1,6 +1,6 @@
-import { Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Image, Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
-import { Feather, Ionicons } from '@expo/vector-icons';
+import { Entypo, Feather, Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import UserAccount from './UserAccount';
 
@@ -8,26 +8,36 @@ const ChatSessionHeader = ({ title }: { title: any }) => {
 
     const router = useRouter();
     return (
-        <View style={{ backgroundColor: "#279EFF", height: Platform.OS == "android" ? 110 : 90, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, paddingTop: Platform.OS == "android" ? 30 : 0 }}>    
+        <View style={{ backgroundColor: "#279EFF", height: Platform.OS == "android" ? 110 : 90, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, paddingTop: Platform.OS == "android" ? 30 : 0 }}>
             <View style={{ padding: 15, flexDirection: "row", justifyContent: "space-between" }}>
-                <Pressable onPress={() => router.back()}>
-                    <Ionicons name="arrow-back-sharp" size={26} color="white" />
-                </Pressable>
+                <View style={{ flexDirection: "row" }}>
+                    <Pressable style={{ marginStart: 10 }} onPress={() => router.back()}>
+                        <Ionicons name="arrow-back-sharp" size={26} color="white" />
+                    </Pressable>
 
-                <View>
-                    <View style={{  }}>
-                        <Text style={{ color: "white", fontSize: 20, marginStart: 5, marginVertical: "auto", fontWeight: "600", textAlign: "center" }}>{title}</Text>
-                        <View style={{flexDirection: "row", marginHorizontal: "auto"}}>
+                    <View style={{ marginStart: "43%" }}>
+                        <View style={{ flexDirection: "row" }}>
+                            <Image
+                                source={require("@/assets/images/avatar.png")}
+                                style={{ width: 30, height: 30, borderRadius: 100, marginVertical: "auto" }}
+                            />
+                            <Text style={{ color: "white", fontSize: 20, marginStart: 5, marginVertical: "auto", fontWeight: "600", textAlign: "center" }}>{title}</Text>
+                        </View>
+                        <View style={{ flexDirection: "row", marginHorizontal: "auto" }}>
                             <Text style={{ color: "#90EE90", marginEnd: 5, fontSize: 10, marginTop: 6 }}>&#9679;</Text>
                             <Text style={{ color: "white", fontSize: 13, textAlign: "center", marginTop: 5 }}>Online</Text>
                         </View>
                     </View>
                 </View>
-                <Pressable style={{ backgroundColor: "white", padding: 5, borderRadius: "100%", marginVertical: "auto" }}
-                // onPress={() => router.navigate("/(session)/userAccount")}
-                >
-                    <UserAccount />
-                </Pressable>
+                <View style={{ flexDirection: "row" }}>
+                    {/* <Ionicons name="call" size={28} color="white" style={{marginEnd: 10, marginTop: 5}} /> */}
+                    <Entypo name="dots-three-vertical" size={24} color="white" style={{ marginTop: 5 }} />
+                    {/* <Pressable style={{ backgroundColor: "white", padding: 5, borderRadius: "100%", marginVertical: "auto" }}
+                    // onPress={() => router.navigate("/(session)/userAccount")}
+                    >
+                        <UserAccount />
+                    </Pressable> */}
+                </View>
             </View>
         </View>
     )
