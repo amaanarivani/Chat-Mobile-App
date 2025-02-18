0aeef83417886b527c7212e865e3fbe75d83af8c
Amaan Alam <amaan@arivani.net>
2025-02-18 16:32:59 +0530

    prod api

--- api/baseUrlConfig.ts
+++ api/baseUrlConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// const baseURL = "https://chat-app-backend-xf7e.onrender.com";
-const baseURL = "http://192.168.1.25:9000";
-// const instance = axios.create({ baseURL: "https://chat-app-backend-xf7e.onrender.com" });
-const instance = axios.create({ baseURL: "http://192.168.1.25:9000" })
+const baseURL = "https://chat-app-backend-xf7e.onrender.com";
+// const baseURL = "http://192.168.1.25:9000";
+const instance = axios.create({ baseURL: "https://chat-app-backend-xf7e.onrender.com" });
+// const instance = axios.create({ baseURL: "http://192.168.1.25:9000" })
 
 export { instance, baseURL };
